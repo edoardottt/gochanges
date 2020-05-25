cae43a4281222a1797a14d0faef693fbfa6e0ee8
edoardottt <edoardott@gmail.com>
2020-05-25 21:29:22 +0200

    test mongoDB

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/edoardottt/gochanges
 
 go 1.13
+
+require go.mongodb.org/mongo-driver v1.3.3
--- scraper/save.go
+++ scraper/save.go
@@ -4,3 +4,13 @@ ADD LICENSE
 
 package scraper
 
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// TODO
+func ConnectDB(connectionString string) (*mongo.Client, error) {
+	client, err := mongo.NewClient(options.Client().ApplyURI(connectionString))
+	return client,err
+}
