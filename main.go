bfb266044a6fa6ba08b85ec8bbc0a993c53d8131
edoardottt <edoardott@gmail.com>
2020-05-26 09:24:36 +0200

    license

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 /*
-ADD LICENSE
+This file is under GNU AFFERO GENERAL PUBLIC LICENSE
+
+Permissions of this strongest copyleft license are conditioned
+on making available complete source code of licensed works and
+modifications, which include larger works using a licensed work,
+under the same license. Copyright and license notices must be preserved.
+Contributors provide an express grant of patent rights.
+When a modified version is used to provide a service over a network,
+the complete source code of the modified version must be made available.
+
+Edoardo Ottavianelli, https://edoardoottavianelli.it
+
  */
 
 package main
