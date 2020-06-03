7e30c5b5e8586f45418143b39b800cff9a050b56
edoardottt <edoardott@gmail.com>
2020-06-03 16:37:37 +0200

    check input

---
+++ webserver/checkinput.go
@@ -0,0 +1,38 @@
+/*
+This file is under GNU AFFERO GENERAL PUBLIC LICENSE
+
+Permissions of this strongest copyleft license are conditioned
+on making available complete source code of licensed works and
+modifications, which include larger works using a licensed work,
+under the same license. Copyright and license notices must be preserved.
+Contributors provide an express grant of patent rights.
+When a modified version is used to provide a service over a network,
+the complete source code of the modified version must be made available.
+
+Edoardo Ottavianelli, https://edoardoottavianelli.it
+
+*/
+
+package webserver
+
+
+
+// TODO
+func checkEmail(email string) bool {
+	return true
+}
+
+// TODO
+func checkWebsite(website string) bool {
+	return true
+}
+
+// TODO
+func checkTelegram(telegram string) bool {
+	return true
+}
+
+// TODO
+func checkInterval(interval string) bool {
+	return true
+}
